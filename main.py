4cd960db53bb71b7927be3deee5a2c8292cd597c
abd2007111 <142683500+abd2007111@users.noreply.github.com>
2023-08-27 16:21:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
-name=input('what is your name')
-age=input('what is your age')
-print(f'my name is{name}and I am{age} years old')
+my_name=input('what is your name')
+my_age=input('what is your age')
+print(f'my name is{my_name}and I am{my_age} years old')
 
 first_number=int(input("first_number"))
 secound_number=int(input ("number two:"))
 operation=input("enter an operation:")
 
-if operation=="+":
-print(first_number+secound_number)
+if operation=='+':
+     print(first_number+secound_number)
 
-elif: operation=="-":
-print(first_number-secound_number)
 
-elif: operation=="*":
-print(first_number*secound_number)
+elif operation=='-':
+    print(first_number-secound_number)
 
-elif: operation=="/":
-print(first_number/secound_number)
+elif operation=='*':
+    print(first_number*secound_number)
+
+elif operation=='/':
+    print(first_number/secound_number)
 else:
-print('the operation is not valid')
+    print('the operation is not valid')
 
 bus_capacity=50
-people_inbus=int(input'hoe many people is in the bus')
+people_inbus=int(input'how many people is in the bus')
 waiting=int(input'how many people are waiting')
-if empty_seats>=waiting:
-    print('join')
-else:
-    print('the bus is full')
+empty_seats=bus_capacity-people_inbus
+if empty_seats > waiting:
+    print('the empty seats is')
+elif empty_seats<= waiting:
+    print('there is mno empty seats')
