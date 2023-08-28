c652dfc19cbf633cac45117b50be2aaccacbfef4
abd2007111 <142683500+abd2007111@users.noreply.github.com>
2023-08-28 20:03:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,12 @@
-my_name=input('what is your name')
-my_age=input('what is your age')
-print(f'my name is{my_name}and I am{my_age} years old')
-
-first_number=int(input("first_number"))
-secound_number=int(input ("number two:"))
-operation=input("enter an operation:")
-
-if operation=='+':
-     print(first_number+secound_number)
-
-
-elif operation=='-':
-    print(first_number-secound_number)
-
-elif operation=='*':
-    print(first_number*secound_number)
-
-elif operation=='/':
-    print(first_number/secound_number)
-else:
-    print('the operation is not valid')
-
-bus_capacity=50
-people_inbus=int(input'how many people is in the bus')
-waiting=int(input'how many people are waiting')
-empty_seats=bus_capacity-people_inbus
-if empty_seats > waiting:
-    print('the empty seats is')
-elif empty_seats<= waiting:
-    print('there is mno empty seats')
+favorite_animals=['dog','cat','monkey','rabbit']
+print(favorite_animals)
+favorite_animals[1]
+favorite_animals.remove('monkey')
+favorite_animals.append("sheep")
+for animal in favorite_animals:
+    print(f"i love {animal}")
+numbers=[1,2,3,4,5]
+numbers_sum=0
+for number in numbers:
+    numbers_sum+=number
+print(f"the total is {numbers_sum}")
